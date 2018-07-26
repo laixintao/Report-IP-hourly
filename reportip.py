400cffc5fd394b16e0494c83bfe3b380b772a67e
laixintao <nanshi.lxt@alipay.com>
2018-07-26 16:49:57 +0800

    porting to python3

--- reportip.py
+++ reportip.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
-#-*-coding:utf8-*-
-
-__author__ = 'laixintao'
+# -*-coding:utf8-*-
 
 import socket
-import fcntl
 import time
-import struct
 import smtplib
-import urllib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
 import re
-import urllib2
+try:
+    from urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
+
+__author__ = "laixintao"
+
 
 # the e-mail config
 # this is just a simple format,this e-mail doesn't exist.
@@ -21,18 +21,19 @@ smtpserver = "smtp.sina.com"
 username = "reaspberrypi@sina.com"
 password = "123456"
 sender = "reaspberrypi@sina.com"
-receiver = ["receiver@sina.com","master@sina.com"]
+receiver = ["receiver@sina.com", "master@sina.com"]
 subject = "[RPI]IP CHANGED"
 
 # file_path config
 file_path = "/root/rootcrons/lastip.txt"
 
+
 def sendEmail(msghtml):
-    msgRoot = MIMEMultipart('related')
-    msgRoot["To"] = ','.join(receiver)
+    msgRoot = MIMEMultipart("related")
+    msgRoot["To"] = ",".join(receiver)
     msgRoot["From"] = sender
-    msgRoot['Subject'] =  subject
-    msgText = MIMEText(msghtml,'html','utf-8')
+    msgRoot["Subject"] = subject
+    msgText = MIMEText(msghtml, "html", "utf-8")
     msgRoot.attach(msgText)
     smtp = smtplib.SMTP()
     smtp.connect(smtpserver)
@@ -44,44 +45,51 @@ def sendEmail(msghtml):
 def check_network():
     while True:
         try:
-            print "Network is Ready!"
+            print("Network is Ready!")
             break
-        except Exception , e:
-           print e
-           print "Network is not ready,Sleep 5s...."
-           time.sleep(10)
+        except Exception as e:
+            print(e)
+            print("Network is not ready,Sleep 5s....")
+            time.sleep(10)
     return True
+
 
 def get_lan_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("1.1.1.1",80))
-    ipaddr=s.getsockname()[0]
+    s.connect(("1.1.1.1", 80))
+    ipaddr = s.getsockname()[0]
     s.close()
     return ipaddr
 
+
 class Getmyip:
+
     def getip(self):
         try:
             myip = self.visit("http://1111.ip138.com/ic.asp")
-        except:
+        except Exception as e:
+            print(e)
             try:
                 myip = self.visit("http://ip.chinaz.com/")
-            except:
+            except Exception as e:
+                print(e)
                 try:
                     myip = self.visit("http://www.whereismyip.com/")
                     # if you want to add more,use the format "except try"
                     # make sure the most useful link be the first
                 except:
-                    print "Fail to get the Network ip."
-                    print "Get the LAN ip."
+                    print("Fail to get the Network ip.")
+                    print("Get the LAN ip.")
                     myip = get_lan_ip()
         return myip
-    def visit(self,url):
-        opener = urllib2.urlopen(url,timeout=20)
+
+    def visit(self, url):
+        opener = urlopen(url)
         if url == opener.geturl():
-            str = opener.read()
-            print "IP information from",url
-        return re.search('\d+\.\d+\.\d+\.\d+',str).group(0)
+            str = opener.read().decode()
+            print("IP information from", url)
+        return re.search("\d+\.\d+\.\d+\.\d+", str).group(0)
+
 
 def get_network_ip():
     getmyip = Getmyip()
@@ -89,21 +97,22 @@ def get_network_ip():
     return localip
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     check_network()
-    ipaddr=get_network_ip()
-    lanip=get_lan_ip()
-    emailip=str(ipaddr)+" "+str(lanip)
+    ipaddr = get_network_ip()
+    lanip = get_lan_ip()
+    emailip = str(ipaddr) + " " + str(lanip)
+    print("Your ip is: ", emailip)
     ip_file = open(file_path)
     last_ip = ip_file.read()
     ip_file.close()
     if last_ip == emailip:
-        print "IP not change."
+        print("IP not change.")
     else:
-        print "IP changed. New ip: {}".format(emailip)
-        ip_file = open(file_path,"w")
+        print("IP changed. New ip: {}".format(emailip))
+        ip_file = open(file_path, "w")
         ip_file.write(str(emailip))
         ip_file.close()
 
         sendEmail(ipaddr)
-        print "Successfully send the e-mail."
+        print("Successfully send the e-mail.")
